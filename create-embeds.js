e6fdb3c624439c6695b2f85a2d8915cbea94ef65
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2022-03-21 06:45:54 +0900

    Greetingで動作停止するバグ直した

--- create-embeds.js
+++ create-embeds.js
@@ -1,22 +1,18 @@
 const Discord = require("discord.js");
 
-exports.createGreetingEmbed = () => new Promise((resolve, reject) => {
-
-    resolve(new Discord.MessageEmbed()
+exports.createGreetingEmbed = () => {
+    return new Discord.MessageEmbed()
         .setTitle('Genshin Tempact')
         .setColor('0x219ddd')
         .setDescription('Version ' + '1.0.0')
         .setURL('https://github.com/Natsu-dev/genshin-tempact')
         .addField(':ballot_box_with_check: About',
-            `毎日5時に原神の武器突破素材、天賦素材の一覧を書き込むbotです。
-            詳しい仕様はGitHubリポジトリ( https://github.com/Natsu-dev/genshin-tempact )に掲載していますので、必ずご一読ください。`,
+            `毎日5時に原神の武器突破素材、天賦素材の一覧を書き込むbotです。`,
             false)
         .addField(':ballot_box_with_check: Commands',
-            `\`gt:info\`, \`gt:i\` このbotの説明が表示されます。`,
+            `\`gt:today\`, \`gt:t\` 今日の天賦素材一覧を表示します。`,
             false)
-    )
-    reject('Error')
-})
+}
 
 exports.createDailyEmbed = (day) => {
     const dayString = ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'][day]
